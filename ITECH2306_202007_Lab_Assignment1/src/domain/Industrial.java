55c85881a667925159df3aecb9dd38e7d83ce137
YangjiSherpa <62415095+YangjiSherpa@users.noreply.github.com>
2020-05-13 14:57:33 +0545

    Code updated

--- ITECH2306_202007_Lab_Assignment1/src/domain/Industrial.java
+++ ITECH2306_202007_Lab_Assignment1/src/domain/Industrial.java
@@ -4,27 +4,66 @@ package domain;
  * @author Yangji
  */
 public class Industrial extends Property {
+	
+	private String hazardType;
+	private String vehicleType;
 
-	private String hazardStatus;
-	private String heavyVehicleStatus;
+	private static final double CIV_RATE = 0.0059; //0.59% this is Given
+	private static final int INDUSTRIAL_WASTE_MANAGEMENT_UNITS = 4;
+	private static final double INDUSTRIAL_WASTE_MANAGEMENT_FEES = 500.00;
+	private static final double FIRE_SERVICE_BASE = 200;
+	private static final double FIRE_SERVICE_PERCENT = 0.00006;
+	private ServiceType industrialWasteManagement;
+	private ServiceType fireServiceLevy;
 	
 	public Industrial() {
-		System.out.println("Not implemented yet");
+		super();
+		// We are explicit about our defaults for Strings
+		this.setHazardType("physical");
+		this.setVehicleType("heavy");
+		setCapitalImprovedRate(CIV_RATE);
+	}
+	public String getHazardType() {
+		return hazardType;
 	}
 
-	@Override
-	public double calculateExtraServices() {
-		// TODO Auto-generated method stub
-		return 0;
+	public void setHazardType(String hazardType) {
+		this.hazardType = hazardType;
 	}
 
+	public String getVehicleType() {
+		return vehicleType;
+	}
+
+	public void setVehicleType(String vehicleType) {
+		this.vehicleType = vehicleType;
+	}
+	
 	@Override
 	public void setUpExtraServices() {
-		// TODO Auto-generated method stub
+		// At this stage,with the given information we are calculating.
+		industrialWasteManagement = new UnitAndRateService("Industrial Waste Management",
+				  INDUSTRIAL_WASTE_MANAGEMENT_UNITS,
+				  INDUSTRIAL_WASTE_MANAGEMENT_FEES);
+		fireServiceLevy = new BaseAndPercentageOfValueService("Fire Levy",
+										FIRE_SERVICE_BASE,
+										FIRE_SERVICE_PERCENT,
+										getCapitalImprovedValue());
+	}
+	
+	@Override
+	public double calculateExtraServices() {
 		
+		return industrialWasteManagement.calculateChargeForServiceType() +
+			   fireServiceLevy.calculateChargeForServiceType();
 	}
 
-
+	@Override
+	public String toString() {
+		return  super.toString() + "Industiral [\n" + 
+							industrialWasteManagement.toString() + "\n" +
+								fireServiceLevy.toString() + " ]\n ";
+	}
 
 	
-}
+
